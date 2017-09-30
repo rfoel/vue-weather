e15f4d1d6a8cfdcbc97c2fab0acd7310d71cfdee
Rafael Franco <rafaelfr@outlook.com>
2017-09-29 21:50:06 -0300

    Added logo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <img src="./assets/images/vue-weather.svg">
+    <find-me></find-me>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import FindMe from './components/FindMe'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    FindMe
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/FindMe.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'find-me',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
